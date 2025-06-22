5da88f036ddd3e22438f47acff36a8e1a527e6fe
Nicholas Yoon <nyoon8360@gmail.com>
2025-06-22 19:37:15 -0400

    1 - Added headerTabs object in layout.js to define header content

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,25 +1,54 @@
 import { getCookie } from "@/utilities/cookieUtility";
 import "./globals.scss";
 import DefaultLanding from "@/backgrounds/default/defaultLanding";
+import BackgroundLake from "@/backgrounds/lake/backgroundLake";
 
 
-export default function Layout({ headerTabs, children }) {
+export default function Layout({ children }) {
+  const headerTabs = [
+    {
+      label: "Home",
+      href: "/"
+    },
+    {
+      label: "Projects",
+      href: "/projects"
+    },
+    {
+      label: "Interests",
+      href: "/interests"
+    },
+    {
+      label: "Music Likes",
+      href: "/music-likes"
+    },
+    {
+      label: "Socials",
+      href: "/socials"
+    }
+  ];
+
   return (
     <html lang="en">
-      {renderBackground(children)}
+      {renderBackground(children, headerTabs)}
     </html>
   );
 }
 
-function renderBackground(children) {
+function renderBackground(children, headerTabs) {
   let selectedBackground = getCookie("nyoon_background");
 
-  if (selectedBackground === null) return (<DefaultLanding/>);
+  if (selectedBackground === null) return <DefaultLanding/>;
 
   switch (selectedBackground) {
     case "lake":
       return(
-        
+        <BackgroundLake headerTabs={headerTabs}>
+          {children}
+        </BackgroundLake>
       );
+
+    default:
+      return <DefaultLanding/>;
   }
 }
